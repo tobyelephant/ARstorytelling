4399c3540a62d75a9f22a61a88654d534fc7051a
CalvinJin525 <2135027@champlaincollege.qc.ca>
2022-08-01 17:48:22 -0400

    Revert "Merge branch 'master' of https://github.com/tobyelephant/ARstorytelling"
    This reverts commit 23888b2d85f535d71f36a8c4e23201955b8302fe, reversing
    changes made to 84740d840f5f7db723819edfb5d65c1942b3f043.

---
+++ StoryTelling2.0/Scene1-4Build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void changeScene::MainMenu()
+extern void changeScene_MainMenu_mCD1B472F0756F22DDD38AB25826819790498F8BE (void);
+// 0x00000002 System.Void changeScene::ARScene()
+extern void changeScene_ARScene_m70C8AAB2C7C93C6E8A1F111EA99DDED2B5E9DF60 (void);
+// 0x00000003 System.Void changeScene::QuitApp()
+extern void changeScene_QuitApp_mB26E13149C9E53279E14DADFB4EC23BAADD77BD1 (void);
+// 0x00000004 System.Void changeScene::.ctor()
+extern void changeScene__ctor_m7232B7E0B2A12A11FC3110C2F21898625318651F (void);
+// 0x00000005 System.Void infoPanel::togglePanel()
+extern void infoPanel_togglePanel_mBDD6B684C1609673BB886D8FB8069F7ACDA4D1CA (void);
+// 0x00000006 System.Void infoPanel::.ctor()
+extern void infoPanel__ctor_m56242BCB7D5C2B2D1B2D96D544A700813DAE8C8C (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	changeScene_MainMenu_mCD1B472F0756F22DDD38AB25826819790498F8BE,
+	changeScene_ARScene_m70C8AAB2C7C93C6E8A1F111EA99DDED2B5E9DF60,
+	changeScene_QuitApp_mB26E13149C9E53279E14DADFB4EC23BAADD77BD1,
+	changeScene__ctor_m7232B7E0B2A12A11FC3110C2F21898625318651F,
+	infoPanel_togglePanel_mBDD6B684C1609673BB886D8FB8069F7ACDA4D1CA,
+	infoPanel__ctor_m56242BCB7D5C2B2D1B2D96D544A700813DAE8C8C,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	6409,
+	6409,
+	6409,
+	6409,
+	6409,
+	6409,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
